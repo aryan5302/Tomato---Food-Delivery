a4fabd3653ea697eeb0ed0279b1e43a4b8a0c667
aryan5302 <vipulshivpur7272@gmail.com>
2025-04-11 02:59:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://aryanrsmtbca:05032002@cluster0.rxabohn.mongodb.net/food-del"
+      ""
     )
     .then(() => console.log("DB Connected"));
 };
